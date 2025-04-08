0ba48fd708f675bd05b2a7e341726f198d376967
Mateus Cizeski Kogl <mateuscizeski@gmail.com>
2025-04-08 10:39:07 -0300

    ajustes

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,5 +1,5 @@
-import React, { createContext, ReactNode, useState } from "react";
-import { destroyCookie, setCookie } from "nookies";
+import React, { createContext, ReactNode, useState, useEffect } from "react";
+import { destroyCookie, setCookie, parseCookies } from "nookies";
 import { useRouter  } from "next/router";
 
 import { api } from "@/services/apiClient";
@@ -56,6 +56,24 @@ export function AuthProvider({ children }: AuthProviderProps) {
     const [user, setUser] = useState<UserProps>();
     const isAuthenticated = !!user;
     const router = useRouter();
+
+    useEffect(() => {
+        const { '@barber.token': token } = parseCookies();
+
+        if(token) {
+            api.get('/me').then((response) => {
+                const { id, name, endereco, email, subscriptions } = response.data;
+                setUser({
+                    id,
+                    name,
+                    email,
+                    endereco,subscriptions
+                });
+            }).catch(() => {
+                signOut();
+            });
+        }
+    }, []);
 
     async function signIn({ email, password }: SignInProps) {
         try {
