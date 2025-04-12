12fd3935c90cfc7002cc92f69a0fcafb0adf88ac
Mateus Cizeski Kogl <mateuscizeski@gmail.com>
2025-04-12 09:54:27 -0300

    complementação do sidebar

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -22,7 +22,13 @@ export function Sidebar({ children }: { children: ReactNode }) {
     return (
         <Box minH="100vh" bg="barber.900">
             <SideBarContent onClose={onClose} display={{ base: 'none', md: 'block' }}/>
-            <Box>
+            <Drawer autoFocus={false} isOpen={isOpen} placement="left" returnFocusOnClose={false} onOverlayClick={onClose} size="full">
+                <DrawerContent>
+                    <SideBarContent onClose={() => onClose()} />
+                </DrawerContent>
+            </Drawer>
+            <MobileNav display={{ base: 'flex', md: 'none'  }} onOpen={onOpen} />
+            <Box ml={{ base: 0, md: 60 }} p={4}>
                 {children}
             </Box>
         </Box>
@@ -71,5 +77,38 @@ const NavItem = ({ icon, children, route, ...rest }: NavItemProps) => {
             {children}
         </Flex>
     </Link>
+    )
+}
+
+interface MobileProps extends FlexProps {
+    onOpen: () => void;
+}
+
+const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
+    return (
+        <Flex 
+            ml={{ base: 0, md: 60}} 
+            px={{ base: 4, md: 24}} 
+            height="20" 
+            alignItems="center" 
+            bg={useColorModeValue('white', 'gray.700')} 
+            borderBottomWidth="1px" 
+            borderBottomColor={useColorModeValue('gray.200', 'gray.700')}
+            justifyContent="flex-start"
+            {...rest}
+        >
+            <IconButton 
+                variant="outline"
+                onClick={onOpen}
+                aria-label="open menu"
+                icon={<FiMenu />}
+            />
+
+            <Flex flexDirection="row">
+                <Text ml={8} fontSize="2x1" fontFamily="monospace" fontWeight="bold">Barber</Text>
+                <Text fontSize="2x1" fontFamily="monospace" fontWeight="bold">PRO</Text>
+            </Flex>
+
+        </Flex>
     )
 }
