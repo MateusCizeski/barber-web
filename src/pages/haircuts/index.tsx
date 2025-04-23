cbbc9e093d23ffba5ae423664b6c765dec856087
Mateus Cizeski Kogl <mateuscizeski@gmail.com>
2025-04-23 10:09:37 -0300

    pagina de listart cortes cadastrados

---
+++ src/pages/haircuts/index.tsx
@@ -0,0 +1,49 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import { Sidebar } from '@/components/sidebar';
+import { Flex, Heading, Text, Stack, Switch, useMediaQuery } from '@chakra-ui/react';
+import { IoMdPricetag } from 'react-icons/io';
+
+export default function Haircuts() {
+    const [isMobile] = useMediaQuery("(max-width: 500px)");
+    
+    return (
+        <>
+            <Head>
+                <title>Modelos de corte - Minha barbearia</title>
+            </Head>
+            <Sidebar>
+                <Flex direction='column' alignItems="flex-start" justifyContent="flex-start">
+                    <Flex direction={isMobile ? "column" : "row"} w='100%' alignItems={isMobile  ? 'flex-start' : 'center'} justifyContent="flex-start" mb={0}>
+                        <Heading fontSize={isMobile ? '28px' :"3xl"} mt={4} mb={4} mr={4} color="orange.900">
+                            Modelos de corte
+                        </Heading>
+                    <Link href="/haircuts/new">
+                        <Button>
+                            Cadastrar novo
+                        </Button>
+                    </Link>
+                        <Stack ml="auto" align="center" direction="row">
+                            <Text fontWeight="bold">Ativos</Text>
+                            <Switch color="green" size="lg" />
+                        </Stack>
+                    </Flex>
+
+                    <Link href="/haircuts/123">
+                        <Flex cursor="pointer" w="100%" p={4} bg="barber.400" direction="row" mb={2} justifyContent="space-between">
+
+                            <Flex direction='row' alignItems="center" justifyContent="center">
+                                <IoMdPricetag size={28} color='#fba931'/>
+                                <Text fontWeight="bold" ml={4} noOfLines={2}>Corte completo</Text>
+                            </Flex>
+
+                            <Text fontWeight="bold">
+                                Preço: R$ 59.90
+                            </Text>
+                        </Flex>
+                    </Link>
+                </Flex>
+            </Sidebar>
+        </>
+    )
+}
