20a48bb75e52fe3cf2ac3543d017c8a8f43d9436
Mateus Cizeski Kogl <mateuscizeski@gmail.com>
2025-04-24 16:39:51 -0300

    recuperar corte da lista

--- src/pages/haircuts/[id].tsx
+++ src/pages/haircuts/[id].tsx
@@ -12,8 +12,28 @@ import  {
 import { Sidebar } from "@/components/sidebar";
 import { FiChevronLeft } from 'react-icons/fi'
 import Link from "next/link";
+import { canSRRAuth } from "@/utils/canSSRAuth";
+import { setupApiClient } from "@/services/api";
 
-export default function EditHaircut() {
+interface HaircutProps {
+    id: string;
+    name: string;
+    price: string | number;
+    status: boolean;
+    user_id: string;
+}
+
+interface SubscriptionsProps {
+    id: string;
+    status: string;
+}
+
+interface EditHaircutProps {
+    haircut: HaircutProps;
+    subscription: SubscriptionsProps;
+}
+
+export default function EditHaircut({ haircut, subscription }: EditHaircutProps) {
     const [isMobile] = useMediaQuery("(max-width: 500px)");
     return (
         <>
@@ -35,13 +55,21 @@ export default function EditHaircut() {
                     >
 
                         <Link href="/haircuts">
-                            <Button mr={3} p={4} display='flex' alignItems='center' justifyContent='center'>
+                            <Button 
+                                sx={{
+                                    mr: 3, 
+                                    p: 4, 
+                                    display: 'flex', 
+                                    alignItems: 'center', 
+                                    justifyContent: 'center',
+                                }} 
+                            >
                                 <FiChevronLeft size={24} color="#fff"/>
                                 Voltar
                             </Button>
                         </Link>
 
-                        <Heading fontSize={isMobile ? '22px' : '3xl'} color={"white"}>Editar corte</Heading>
+                        <Heading fontSize={isMobile ? '22px' : '3xl'} sx={{ color: "white" }} >Editar corte</Heading>
                     </Flex>
 
                     <Flex mt={4} maxW={'700px'} pt={8} pb={8} w={'100%'} bg={'barber.400'} direction={'column'} align={'center'} justify={'center'}>
@@ -49,37 +77,88 @@ export default function EditHaircut() {
 
                         <Flex w={'85%'} direction={"column"}>
                             <Input 
-                                placeholder="Nome do corte"
-                                bg={"gray.900"}
-                                mb={3}
-                                size={'lg'}
-                                type="text"
-                                w={'100%'}
+                                sx={{
+                                    placeholder: "Nome do corte",
+                                    bg: "gray.900",
+                                    mb: 3,
+                                    size: 'lg',
+                                    type: "text",
+                                    w: '100%'
+                                }}
                             />
 
                             <Input 
-                                placeholder="valor do corte"
-                                bg={"gray.900"}
-                                mb={3}
-                                size={'lg'}
-                                type="number"
-                                w={'100%'}
+                                sx={{
+                                    placeholder: "Valor do corte",
+                                    bg: "gray.900",
+                                    mb: 3,
+                                    size: 'lg',
+                                    type: "number",
+                                    w: '100%'
+                                }}
                             />
 
-                            <Stack mb={6} align={'center'} direction={'row'}>
-                                <Text fontWeight={'bold'}>Desativar corte</Text>
+                            <Stack 
+                                sx={{
+                                    mb: 6, 
+                                    align: 'center', 
+                                    direction: 'row'
+                                }} 
+                            >
+                                <Text sx={{ fontWeight: 'bold' }} >Desativar corte</Text>
 
-                                <Switch size={'lg'} colorScheme="red"/>
+                                <Switch sx={{ size: 'lg', colorScheme: "red" }} />
                             </Stack>
 
-                            <Button mb={6} w={'100%'} bg={'button.cta'} color={'gray.900'} _hover={{ bg: "#ffb13e" }}>
+                            <Button mb={6} w={'100%'} bg={'button.cta'} color={'gray.900'} _hover={{ bg: "#ffb13e" }} disabled={subscription?.status !== 'active'}>
                                 Salvar
                             </Button>
+
+                            { subscription?.status !== 'active' && (
+                                <Flex direction={'row'} align={'center'} justify={'center'}>
+                                    <Link href='/planos'>
+                                        <Text fontWeight={'bold'} mr={1} color={'#31fb6a'} cursor={'pointer'}>Seja premium</Text>
+                                    </Link>
+                                    <Text>
+                                        e tenha todos acessos liberados
+                                    </Text>
+                                </Flex>
+                            )}
                         </Flex>
                     </Flex>
-
                 </Flex>
             </Sidebar>
         </>
     )
 }
+
+export const getServerSideProps = canSRRAuth(async (ctx) => {
+    const { id } = ctx.params;
+
+    try {
+        const apiClient = setupApiClient(ctx);
+        const check = await apiClient.get('/haircut/check');
+        const response = await apiClient.get('/haircut/detail', {
+            params: {
+                haircut_id: id
+            }
+        });
+
+        return {
+            props: {
+                haircut: response.data,
+                subscription: check.data?.subscriptions
+            }
+        }
+    }
+    catch(err) {
+        console.log(err);
+
+        return {
+            redirect: {
+                destination: '/dashboard',
+                permanent: false
+            }
+        }
+    }
+})
