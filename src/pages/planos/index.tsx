0540b8e43843352f7a73e39fd0ac125b5f0cab37
Mateus Cizeski Kogl <mateuscizeski@gmail.com>
2025-04-27 09:54:51 -0300

    Completação da página de planos.

--- src/pages/planos/index.tsx
+++ src/pages/planos/index.tsx
@@ -7,8 +7,14 @@ import {
   useMediaQuery
 } from '@chakra-ui/react';
 import { Sidebar } from '@/components/sidebar';
+import { canSRRAuth } from '@/utils/canSSRAuth';
+import { setupApiClient } from '@/services/api';
 
-export default function Planos() {
+interface PlanosProps {
+  premium: boolean;
+}
+
+export default function Planos({ premium }: PlanosProps) {
   const [isMobile] = useMediaQuery(['(max-width: 500px)']);
 
   return (
@@ -24,7 +30,6 @@ export default function Planos() {
 
         <Flex pb={8} maxW='700px' w='100%' direction='column' align='flex-start' justify='flex-start'>
           <Flex gap={4} w='100%' flexDirection={isMobile ? 'column' : 'row'}>
-
             <Flex rounded={4} p={2} flex={1} bg='barber.400' flexDirection='column'>
               <Heading textAlign='center' fontSize='2xl' mt={4} color='gray.100'>
                 Plano grátis
@@ -34,11 +39,9 @@ export default function Planos() {
               <Text fontWeight='medium' ml={4} mb={2}>Criar apenas 3 modelos de corte.</Text>
               <Text fontWeight='medium' ml={4} mb={2}>Editar dados do perfil.</Text>
             </Flex>
-
           </Flex>
 
           <Flex gap={4} w='100%' flexDirection={isMobile ? 'column' : 'row'}>
-
             <Flex rounded={4} p={2} flex={1} bg='barber.400' flexDirection='column'>
               <Heading textAlign='center' fontSize='2xl' mt={4} color='#31fb6a'>
                 Premium
@@ -51,12 +54,38 @@ export default function Planos() {
               <Text fontWeight='medium' ml={4} mb={2}>Receber todas as atualizações</Text>
               <Text color='#31fb6a' fontWeight='bold' fontSize='2xl' ml={4} mb={2}>R$ 9.99</Text>
 
-              <Button bg='button.cta' m={2} color='white' onClick={() => {}}>Virar premium</Button>
-            </Flex>
+              <Button bg='button.cta' m={2} color='white' onClick={() => {}}>{premium ? 'Você já é premium' : 'Virar premium'}</Button>
 
+              {premium && (
+                <Button m={2} bg='white' color='barber.900' fontWeight='bold' onClick={() => {}}>Alterar assinatura</Button>
+              )}
+            </Flex>
           </Flex>
         </Flex>
       </Sidebar>
     </>
   )
 }
+
+export const getServerSideProps = canSRRAuth(async (ctx) => {
+  try {
+    const apiClient = setupApiClient(ctx);
+    const response = await apiClient.get('/me');
+
+    return {
+      props: {
+        premium: response.data?.subscriptions?.status === 'active' ? true : false
+      }
+    }
+  }
+  catch(err) {
+    console.log(err);
+
+    return {
+      redirect: {
+        destination: '/dashboard',
+        permanent: false
+      }
+    }
+  }
+});
